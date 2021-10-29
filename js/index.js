2c57ed049f38bd249f714a59e11c6ca180d40405
Alex <Eyesless1jack@gmail.com>
2021-10-29 16:55:44 +0300

    Made list massive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
-const wrap = document.querySelector('.wrapper');
+const list = document.querySelector('#ToDoList');
 
 const form = document.querySelector('form');
 const CrtInpt = document.querySelector('#CrtInpt');
 const CrtDate = document.querySelector('#CrtDate');
 
 const t = document.querySelector('#ToDoEl');
-const Num = t.content.querySelector('#Num');
+const Num = t.content.querySelector('label');
 const Do = t.content.querySelector('#Do');
 const Dt = t.content.querySelector('#Date');
 const ComBtn = t.content.querySelector('#Complete');
 const DelBtn = t.content.querySelector('#Delete');
 
-form.addEventListener('submit',function() {
-    let El = t.content.cloneNode(true); 
-    Do.textContent = CrtInpt.Value;
-    Dt.textContent = CrtDate.Value;
-    wrap.append(El);
-    alert(El);
+let mas = [];
+let El;
+
+form.addEventListener('submit',function() { 
+    mas.push([CrtInpt.Value,CrtDate.Value]);
 });
+
+for (let i=0;i<mas.length;i++){
+    Num.textContent = i+1;
+    Do.textContent = mas[i][0];
+    Dt.textContent = mas[i][1];
+    El = t.content.cloneNode(true);
+    list.append(El);
+}
+
+/*Num.textContent = '1';
+Do.textContent = 'sgsggs';
+Dt.textContent = '11.11.1111';
+El = t.content.cloneNode(true);
+list.append(El);*/
