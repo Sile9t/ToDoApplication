6336b97e8f6327fbc59e5f55ad54ff340b1dbe75
Alex <Eyesless1jack@gmail.com>
2021-11-01 12:10:08 +0300

    Srtucting code

--- js/index.js
+++ js/index.js
@@ -4,26 +4,34 @@ const form = document.querySelector('form');
 const CrtInpt = document.querySelector('#CrtInpt');
 const CrtDate = document.querySelector('#CrtDate');
 
-const t = document.querySelector('#ToDoEl');
-const Num = t.content.querySelector('label');
-const Do = t.content.querySelector('#Do');
-const Dt = t.content.querySelector('#Date');
-const ComBtn = t.content.querySelector('#Complete');
-const DelBtn = t.content.querySelector('#Delete');
+const ToDoEl = document.querySelector('#ToDoEl');
+const Num = ToDoEl.content.querySelector('label');
+const Do = ToDoEl.content.querySelector('#Do');
+const Dt = ToDoEl.content.querySelector('#Date');
+const ComBtn = ToDoEl.content.querySelector('#Complete');
+const DelBtn = ToDoEl.content.querySelector('#Delete');
 
 let mas = [];
 let El;
 
-form.addEventListener('submit',function() { 
-    mas.push([CrtInpt.Value,CrtDate.Value]);
-});
-
-mas.push(['sgawgag','11.11.1111']); 
-
-for (let i=0;i<mas.length;i++){
-    Num.textContent = i+1;
-    Do.textContent = mas[i][0];
-    Dt.textContent = mas[i][1];
-    El = t.content.cloneNode(true);
-    list.append(El);
+let UpdateList = function (){
+    for (let i=0;i<mas.length;i++){
+        Num.textContent = i+1;
+        Do.textContent = mas[i].text;
+        Dt.textContent = mas[i].date;
+        El = ToDoEl.content.cloneNode(true);
+        list.append(El);
+    }
 }
+
+let AddToList = function (t,d){
+    let el = { text: t, date: d};
+    mas.push(el);
+    UpdateList();
+}
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault(); 
+    AddToList(CrtInpt.value,CrtDate.value);
+    console.log(list.firstElementChild.firstChild);
+});
