bede27ed968c526fb25274c96c2d0ecf161c18b8
Alex <Eyesless1jack@gmail.com>
2021-11-02 20:24:04 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ const form = document.querySelector('form');
 const CrtInpt = document.querySelector('#CrtInpt');
 const CrtDate = document.querySelector('#CrtDate');
 
-const ToDoEl = document.querySelector('#ToDoEl');
+/*const ToDoEl = document.querySelector('#ToDoEl');
 const Num = ToDoEl.content.querySelector('label');
 const DoText = ToDoEl.content.querySelector('#Do');
 const Dodate = ToDoEl.content.querySelector('#Date');
 const ComBtn = ToDoEl.content.querySelector('#Complete');
-const DelBtn = ToDoEl.content.querySelector('#Delete');
+const DelBtn = ToDoEl.content.querySelector('#Delete');*/
 
 let mas = [];
 let El;
@@ -17,11 +17,43 @@ let El;
 let UpdateList = function () {
     if (list.childElementCount != 0) {list.innerHTML = '';}
     for (let i=0;i<mas.length;i++){
-        Num.textContent = i+1;
+        let div = document.createElement('div');
+        div.className = 'ToDos';
+        let Num = document.createElement('label');
+        Num.innerHTML = "'"+(i+1)+"'";
+        let DoText = document.createElement('label');
+        DoText.innerHTML = mas[i].text;
+        let DoDate = document.createElement('label');
+        DoDate.innerHTML = mas[i].date;
+        let ComBtn = document.createElement('button');
+        ComBtn.id = 'Complete';
+        ComBtn.className = 'ListBtn';
+        ComBtn.onclick = function() {
+            if (this.innerHTML == 'Complete') {this.innerHTML = ''}
+        };
+        let DelBtn = document.createElement('button');
+        DelBtn.id = 'Delete';
+        DelBtn.className = 'ListBtn';
+        DelBtn.onclick = function() {
+            if (ComBtn.innerHTML == 'Complete') {
+                for (let i = this.parentElement.firstElementChild.innerHTML - 1;i<mas.length;i++){
+                    if (i<mas.length) {mas[i].text = mas[i+1].text; mas[i].date = mas[i+1].date;}
+                    else {mas.pop();}
+                }
+                UpdateList();
+            }
+            else {alert('ToDo need to be completed!');}
+        };
+        div.append(Num);
+        div.append(DoText);
+        div.append(DoDate);
+        div.append(ComBtn);
+        div.append(DelBtn);
+        /*Num.textContent = i+1;
         DoText.textContent = mas[i].text;
         Dodate.textContent = mas[i].date;
         El = ToDoEl.content.cloneNode(true);
-        list.append(El);
+        list.append(El);*/
     }
 }
 
