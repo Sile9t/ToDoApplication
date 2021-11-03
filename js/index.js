61fbb573206cc4c14cd99f4169819492eac94b9a
Alex <Eyesless1jack@gmail.com>
2021-11-03 14:50:06 +0300

    Delete unuseable code

--- js/index.js
+++ js/index.js
@@ -4,13 +4,6 @@ const form = document.querySelector('form');
 const CrtInpt = document.querySelector('#CrtInpt');
 const CrtDate = document.querySelector('#CrtDate');
 
-/*const ToDoEl = document.querySelector('#ToDoEl');
-const Num = ToDoEl.content.querySelector('label');
-const DoText = ToDoEl.content.querySelector('#Do');
-const Dodate = ToDoEl.content.querySelector('#Date');
-const ComBtn = ToDoEl.content.querySelector('#Complete');
-const DelBtn = ToDoEl.content.querySelector('#Delete');*/
-
 let mas = [];
 let El;
 
@@ -20,40 +13,25 @@ let UpdateList = function () {
         let div = document.createElement('div');
         div.className = 'ToDos';
         let Num = document.createElement('label');
-        Num.innerHTML = "'"+(i+1)+"'";
+        Num.innerHTML = i+1;
         let DoText = document.createElement('label');
         DoText.innerHTML = mas[i].text;
         let DoDate = document.createElement('label');
         DoDate.innerHTML = mas[i].date;
         let ComBtn = document.createElement('button');
+        ComBtn.innerHTML = 'Not complete';
         ComBtn.id = 'Complete';
         ComBtn.className = 'ListBtn';
-        /*ComBtn.onclick = function() {
-            if (this.innerHTML == 'Complete') {this.innerHTML = ''}
-        };*/
         let DelBtn = document.createElement('button');
+        DelBtn.innerHTML = 'Delete';
         DelBtn.id = 'Delete';
         DelBtn.className = 'ListBtn';
-        /*DelBtn.onclick = function() {
-            if (ComBtn.innerHTML == 'Complete') {
-                for (let i = this.parentElement.firstElementChild.innerHTML - 1;i<mas.length;i++){
-                    if (i<mas.length) {mas[i].text = mas[i+1].text; mas[i].date = mas[i+1].date;}
-                    else {mas.pop();}
-                }
-                UpdateList();
-            }
-            else {alert('ToDo need to be completed!');}
-        };*/
         div.append(Num);
         div.append(DoText);
         div.append(DoDate);
         div.append(ComBtn);
         div.append(DelBtn);
-        /*Num.textContent = i+1;
-        DoText.textContent = mas[i].text;
-        Dodate.textContent = mas[i].date;
-        El = ToDoEl.content.cloneNode(true);
-        list.append(El);*/
+        list.append(div);
     }
 }
 
@@ -66,41 +44,27 @@ let AddToList = function (txt,dte) {
 form.addEventListener('submit', function(e) {
     e.preventDefault(); 
     AddToList(CrtInpt.value,CrtDate.value);
-    //console.log(list.firstElementChild.firstElementChild.nextElementSibling.innerHTML);
-    //console.log(list.remove(list.firstElementChild.firstElementChild));
 });
-
-/*ComBtn.addEventListener('click', function() {
-    if (this.innerHTML == 'Complete') {this.innerHTML = 'Not complete';}
-    else {this.innerHTML = 'Complete';}
-});
-
-DelBtn.addEventListener('click', function() {
-    if (this.parentElement.lastElementChild.prevElementSibling.innerHTML == 'Complete') {
-        for (let i = this.parentElement.firstElementChild.innerHTML - 1;i<mas.length;i++){
-            if (i<mas.length) {mas[i].text = mas[i+1].text; mas[i].date=mas[i+1].date;}
-            else {mas.pop();}
-        }
-        UpdateList();
-    }
-    else {prompt('ToDo need to be completed!');}
-});*/
 
 list.onclick = function(event) {
     let target = event.target;
-    if (target.id = 'Complete') {ChangeCom(target);}
-    else if (target.id = 'Delete') {DelEl(target);}
+    if (target.id == 'Complete') {ChangeCom(target);}
+    else if (target.id == 'Delete') {DelEl(target);}
     else return;
 }
 
 let ChangeCom = function(Com) {
-    if (Com.innerHTML = 'Complete') {Com.innerHTML = 'Not complete';}
+    if (Com.innerHTML == 'Complete') {Com.innerHTML = 'Not complete';}
     else {Com.innerHTML = 'Complete';}
 }
 
 let DelEl = function(Del) {
-    if (Del.parentElement.lastElementChild.prevElementSibling.innerHTML = 'Complete'){
-        for (let i = Del.parentElement.firstElementChild.innerHTML - 1;i<mas.length;i++){
+    console.log(Del.parentElement.lastElementChild.prevElementSibling.id);
+    console.log(Del.parentElement.firstElementChild.innerHTML);
+    if (Del.parentElement.lastElementChild.prevElementSibling.innerHTML == 'Complete'){
+        console.log(Del.parentElement.lastElementChild.prevElementSibling);
+        console.log(Del.parentElement.firstElementChild.innerHTML);
+        for (let i = Number(Del.parentElement.firstElementChild.innerHTML) - 1;i<mas.length;i++){
             if (i<mas.length) {mas[i].text = mas[i+1].text; mas[i].date=mas[i+1].date;}
             else {mas.pop();}
         }
